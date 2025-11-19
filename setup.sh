cbdd8fef760f01720fe948f5a07be6f29ef0c038
robert <robert@rob-deb.rob-deb>
2025-11-19 20:41:33 +0100

    Initial commit.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install ansible
+
+
+sudo apt install git
+
+# SSH key stuff
+
+git clone git@github.com:RobertVDLeeuw/ansible
+
